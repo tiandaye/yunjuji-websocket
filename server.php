dba85995d9763aeb9109a9903c391999f2854c16
tiandaye <q408596288@vip.qq.com>
2017-10-26 15:29:00 +0800

    commit

--- server.php
+++ server.php
@@ -4,10 +4,11 @@
  * @Author: admin
  * @Date:   2017-10-26 14:36:22
  * @Last Modified by:   admin
- * @Last Modified time: 2017-10-26 15:12:53
+ * @Last Modified time: 2017-10-26 15:28:30
  */
+
 require_once __DIR__.'/vendor/autoload.php';
 
-$config = require_once './configs/yunjuji.php';
+$config = require_once __DIR__.'/configs/yunjuji.php';
 
 new Yunjuji\WebSocket\WebSocket($config);
